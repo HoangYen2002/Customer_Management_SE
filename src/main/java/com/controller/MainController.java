c1b8f41dd1750c5bbfc8fed1d546ff56bb9cf312
HoangYen2002 <hoangyen.fw397@gmail.com>
2022-11-11 22:16:25 +0700

    Done Com.Service and Com.Service.Impl

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -1,5 +1,33 @@
 package com.controller;
 
-public class MainController {
+import java.util.Date;
+import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.model.Customer;
+import com.service.impl.CustomerServiceImpl;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+	
+	
+	
+	
+	
 }
