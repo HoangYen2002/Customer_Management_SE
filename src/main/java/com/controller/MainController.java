f33dcbb2c7e847d57d32c60b6d0ad34a33a4f9a0
HoangYen2002 <hoangyen.fw397@gmail.com>
2022-11-12 09:53:45 +0700

    Add Find, Delete, GetAll in MainController

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -26,7 +26,38 @@ import com.service.impl.CustomerServiceImpl;
 @RequestMapping("/main")
 public class MainController {
 	
+	@Autowired
+	@Qualifier("customerServiceImpl")
+	private CustomerServiceImpl customerService;
 	
+	@RequestMapping(value = {"customer/list"}, method = RequestMethod.GET)
+	@ResponseBody
+	public List <Customer> getAllCustomer(){
+		List<Customer> customer = customerService.getAllCustomer();
+		if(customer == null) {
+			System.out.println("Not found");
+		}
+		return customer;
+	}
+	
+	@GetMapping(value = {"find/{id}"})
+	@ResponseBody
+	public Customer findAllCustomer(@PathVariable("id") int id) {
+		Customer customer = customerService.findAllCustomer(id);
+		if(customer == null) {
+			System.out.println("Not found");
+		}
+		return customer;
+	}
+	
+	@DeleteMapping(value = {"customer/delete/{id}"})
+	public String deleteCustomer(@PathVariable("id") int id) {
+		boolean isDeleted = customerService.deleteCustomer(id);
+		if(isDeleted) {
+			System.out.println("Delete Customer Successfully.");
+		}
+		return "redirect:/main/customer/list";
+	}
 	
 	
 	
