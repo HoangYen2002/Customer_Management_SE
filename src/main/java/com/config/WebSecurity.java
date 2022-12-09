c2ae19c7819d652403968cdc1a6a532b1a793c13
HoangYen2002 <hoangyen.fw397@gmail.com>
2022-12-09 15:42:26 +0700

    Add Web Security Config

---
+++ src/main/java/com/config/WebSecurity.java
@@ -0,0 +1,53 @@
+package com.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.authentication.MyDBAuthenticationService;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	@Qualifier("myDBAuthenticationService")
+	private MyDBAuthenticationService myDBAauthenticationService;
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable(); // khong co dong nay se luon nhay vao /403
+		http.authorizeRequests().antMatchers("customer/list").access("hasRole('MANAGER')");
+		http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
+		http.authorizeRequests().and().formLogin()
+		.loginProcessingUrl("/j_spring_security_check")
+		.loginPage("/login")
+		.defaultSuccessUrl("/accountInfo")
+		.failureUrl("/login?error=true")
+		.usernameParameter("userName")
+		.passwordParameter("password")
+		.and().logout()
+		.logoutUrl("/logout")
+		.logoutSuccessUrl("/");
+	}
+	
+	@Autowired
+	protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		//auth.inMemoryAuthentication().withUser("employee1").password("{noop}password1").roles("EMPLOYEE");
+		auth.inMemoryAuthentication().withUser("manager1").password("{noop}password2").roles("MANAGER");
+		//auth.userDetailsService(myDBAauthenticationService).passwordEncoder(bCryptPasswordEncoder());
+	}
+}
