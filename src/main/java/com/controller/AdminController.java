e849a3623434d1058ab725457ae9c706f3feb93f
HoangYen2002 <hoangyen.fw397@gmail.com>
2022-12-10 16:13:48 +0700

    Fixed Admin Controller

--- src/main/java/com/controller/AdminController.java
+++ src/main/java/com/controller/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,10 @@ public class AdminController {
 		return "/403";
 	}
 	
+	@GetMapping(value = { "/login" })
+	public String login(Model model) {
+		return "login";
+	}
 	
 	// ADMIN CO QUYEN XEM HET DANH SACH CUSTOMER
 	@RequestMapping(value = {"customer/list"}, method = RequestMethod.GET)
